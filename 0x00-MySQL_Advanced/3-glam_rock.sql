d6f22ee7ace9178726eea1fce053e8422f444975
EugeneJoe <jmuthui70@gmail.com>
2021-12-10 17:08:51 +0000

    Update to use LOCATE function

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,5 @@
 -- select bands with Glam Rock as their style, ranked by longevity
-SELECT band_name, IFNULL(split, 2021) - formed AS lifespan FROM metal_bands WHERE style like '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, IFNULL(split, 2021) - formed AS lifespan
+FROM metal_bands
+WHERE LOCATE('Glam rock', style)
+ORDER BY lifespan DESC;
