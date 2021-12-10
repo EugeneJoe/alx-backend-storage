4bb0e5afe37c9373ed2a6e6991f2fbe531915a6d
EugeneJoe <jmuthui70@gmail.com>
2021-12-10 17:13:19 +0000

    Use create index statement

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
 -- create an index 'idx_name_first' on table names and the first letter of 'name'
-alter TABLE names ADD INDEX idx_name_first ON names (name(1));
+CREATE INDEX idx_name_first ON names (name(1));
