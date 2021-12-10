a10338ae62c264776b33c2b0267d1f718f7a8e36
EugeneJoe <jmuthui70@gmail.com>
2021-12-10 17:28:11 +0000

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- select bands with Glam Rock as their style, ranked by longevity
-SELECT DISTINCT band_name, IFNULL(split, 2021) - formed AS lifespan
+SELECT DISTINCT band_name, IFNULL(split, 2020) - formed AS lifespan
 FROM metal_bands
-WHERE LOCATE('Glam rock', style)
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
